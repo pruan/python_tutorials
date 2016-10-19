c1e0e7db4eaf7ae5fc7423df769581f4fb1f5277
Peter Ruan <pruan@redhat.com>
2016-10-18 17:42:08 -0700

    raw_input tutorial

---
+++ reading_raw_input.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Task
+Read a line of input from stdin and save it to a variable, . Then print the contents of  to stdout.
+
+Input Format
+
+A single line containing sentence .
+
+Constraints
+
+Output Format
+
+Print the contents of  to stdout.
+Sample Input
+
+How many chickens does it take to cross the road?
+Sample Output
+
+How many chickens does it take to cross the road?
+"""
+
+input = raw_input("How many chickens does it take to cross the road? ")
+print "%s" % input
+
